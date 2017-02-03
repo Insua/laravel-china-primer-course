4524f4e50897b46c9b3f88d92b968ac866793500
insua <insuaxe@gmail.com>
2017-02-03 16:40:20 +0800

    commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="http://ok119ljzh.bkt.clouddn.com/%E9%AD%85%E5%8A%9B%E4%B9%A1%E6%9D%91.jpg">
+            <a href="#">
+                SLOGON
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
