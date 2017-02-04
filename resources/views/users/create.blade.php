aa3782e638feb9503e2078e194a26e088ed5a75f
insua <insuaxe@gmail.com>
2017-02-04 16:12:43 +0800

    commit

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -18,7 +18,6 @@
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
                     </div>
 
-
                     <div class="form-group">
                         <label for="email">邮箱：</label>
                         <input type="text" name="email" class="form-control" value="{{ old('email') }}">
