a19cdecdc52d8b5c3999c38e3a88d59a3487dce9
insua <insuaxe@gmail.com>
2017-02-24 08:56:51 +0800

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -35,6 +36,7 @@ class UsersController extends Controller
             'password' => $request->password,
         ]);
 
+        Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
     }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,7 +16,7 @@
                                 <li class="divider"></li>
                                 <li>
                                     <a href="#" id="logout">
-                                        <form action="{{ route('logout') }}">
+                                        <form action="{{ route('logout') }}" method="POST">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
                                             <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
