e8f7cc6faf21f51a957cee9af3a705a02d882259
insua <insuaxe@gmail.com>
2017-02-24 16:54:29 +0800

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,13 @@ use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'only' => ['edit','update']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
