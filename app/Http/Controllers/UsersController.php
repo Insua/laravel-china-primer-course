814cf214ee2032d66db38130020ac2fb79c12343
insua <insuaxe@gmail.com>
2017-02-24 15:01:24 +0800

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,7 @@ class UsersController extends Controller
         $this->validate($request,[
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
 
         $user = User::create([
@@ -45,5 +45,27 @@ class UsersController extends Controller
     {
         $user = User::findOrFail($id);
         return view('users.edit',compact('user'));
+    }
+
+    public function update($id,Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'confirmed|min:6'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password)
+        {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update();
+
+        session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$id);
     }
 }
