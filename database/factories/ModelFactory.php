32a0268074911dc090a433b051fc03eae38d271d
insua <insuaxe@gmail.com>
2017-04-29 11:55:08 +0800

    commit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => $password ? : $password = bcrypt('secret'),
         'remember_token' => str_random(10),
-        'create_at' => $date_time,
-        'update_at' => $date_time,
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
