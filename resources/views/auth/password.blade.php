badbc0f7d76b51d8aa313809ce2ef58c75469d5a
insua <insuaxe@gmail.com>
2017-05-02 08:41:11 +0800

    commit

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -2,5 +2,26 @@
 @section('title','重置密码')
 
 @section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">重置密码</div>
+                <div class="panel-body">
 
+                    @include('shared.errors')
+
+                    <form method="POST" action="{{ route('password.reset') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
