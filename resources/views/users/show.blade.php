9317dd81682de1b2e9bc52f34959466f572fddba
insua <insuaxe@gmail.com>
2017-05-02 16:58:53 +0800

    commit

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,11 +6,14 @@
     <div class="row">
         <div class="col-md-offset-2 col-md-8">
             <div class="col-md-12">
-                <div class="col-md-offset-2 col-md-8">
-                    <section class="user_info">
-                        @include('shared.user_info',['user'=>$user])
-                    </section>
-                </div>
+                @if(count($statuses) > 0)
+                    <ol class="statuses">
+                        @foreach($statuses as $status)
+                            @include('statuses._status')
+                        @endforeach
+                    </ol>
+                    {!! $statuses->render() !!}
+                @endif
             </div>
         </div>
     </div>
