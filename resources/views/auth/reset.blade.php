95ab28f40312047f0d41d68827813cdc8f301cb8
insua <insuaxe@gmail.com>
2017-05-02 11:44:37 +0800

    commit

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -14,6 +14,36 @@
                         <form method="POST" action="{{ route('password.update') }}">
                             {{ csrf_field() }}
                             <input type="hidden" name="token" value="{{ $token }}">
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">邮箱地址：</label>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">密码：</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">确认密码：</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control" name="password_confirm">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        更新密码
+                                    </button>
+                                </div>
+                            </div>
+
                         </form>
 
                     </div>
